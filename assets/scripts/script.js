be0b98b56563ab822b32591f3e01cc97127081b8
Zach LaFleur <zach.lafleur95@gmail.com>
2020-08-05 20:56:21 -0600

    added constants and prompts to ask user what should be included in their password

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  const passwordLength = prompt("How many characters do you want in your password, with a minimum of 8 characters?");
+  const wantSymbols = confirm("Would you like symbols in your password?");
+  const wantNumb = confirm("Would you like numbers in your password?");
+  const wantUpper = confirm("Would you like uppercase Letters in your password?");
+  const wantLower = confirm("Would you like lowercase letters in your password?")
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
---
+++ psuedo.md
@@ -0,0 +1,21 @@
+user clicks on generate password button
+
+collect user input for password criteria
+* length
+    * minimum 8 characters
+    * maximum 128 characters
+* symbols
+    * what type of symbols should be included?
+* numbers
+* uppercase letters
+* lowercase letters
+
+randomize user input and organize it
+
+select random characters in each category
+* random numbers
+* random symbols
+* random characters
+
+display password on screen
+
